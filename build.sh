2094b253a0f9f14e8acda1a7e863dff8aeb3a549
Patrick Dawkins <patrick@platform.sh>
2021-03-17 12:02:43 +0000

    Fix undefined $version

--- build.sh
+++ build.sh
@@ -5,18 +5,21 @@ if [ -z "$filename" ]; then
   echo >&2 "Usage: ./build.sh [filename] [version-ref]"
   exit 1
 fi
-tag="$(cat latest-tag)"
-tag=${2:-"$tag"}
+version=$2
 if [ -d cli ]; then
     rm -rf cli
 fi
-git clone --branch "$tag" https://github.com/platformsh/platformsh-cli.git cli
+if [ -n "$version" ]; then
+    git clone --branch "$version" https://github.com/platformsh/platformsh-cli.git cli
+else
+    git clone https://github.com/platformsh/platformsh-cli.git cli
+    export version="$(git describe --tags)"
+fi
 cp config.yaml cli/config.yaml
 cd cli/vendor-bin/box
 composer install --no-interaction
 cd -
 cd cli
-export version="$(git describe --tags)"
 composer install --no-dev --no-interaction
 mkdir -p vendor/bin
 ln -s "$(realpath vendor-bin/box/vendor/bin/box)" vendor/bin/box
